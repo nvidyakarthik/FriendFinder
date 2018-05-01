8f0dae4fc25027984d9037b33955ad9750a29f09
nvidya.karthik <nvidya.karthik@gmail.com>
2018-05-01 09:25:52 -0700

    more dummy data added for testing

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,25 +5,249 @@
 // But you could have it be an empty array as well.
 // ===============================================================================
 
-var friends = [
+var friends = 
+  [
     {
-        "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-          ]
-      }
-      
-  ];
+    "name": "Ahmed",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+    "scores": [
+    "5",
+    "1",
+    "4",
+    "4",
+    "5",
+    "1",
+    "2",
+    "5",
+    "4",
+    "1"
+    ]
+    },
+    {
+    "name": "Jacob Deming",
+    "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+    "scores": [
+    "4",
+    "2",
+    "5",
+    "1",
+    "3",
+    "2",
+    "2",
+    "1",
+    "3",
+    "2"
+    ]
+    },
+    {
+    "name": "Jeremiah Scanlon",
+    "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+    "scores": [
+    "5",
+    "2",
+    "2",
+    "2",
+    "4",
+    "1",
+    "3",
+    "2",
+    "5",
+    "5"
+    ]
+    },
+    {
+    "name": "Louis T. Delia",
+    "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+    "scores": [
+    "3",
+    "3",
+    "4",
+    "2",
+    "2",
+    "1",
+    "3",
+    "2",
+    "2",
+    "3"
+    ]
+    },
+    {
+    "name": "Lou Ritter",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+    "scores": [
+    "4",
+    "3",
+    "4",
+    "1",
+    "5",
+    "2",
+    "5",
+    "3",
+    "1",
+    "4"
+    ]
+    },
+    {
+    "name": "Jordan Biason",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+    "scores": [
+    "4",
+    "4",
+    "2",
+    "3",
+    "2",
+    "2",
+    "3",
+    "2",
+    "4",
+    "5"
+    ]
+    },
+    {
+    "name": "fofofof",
+    "photo": "https://www.pexels.com/photo/grey-and-white-short-fur-cat-104827/",
+    "scores": [
+    "1",
+    "1",
+    "1",
+    "1",
+    "3",
+    "2",
+    "3",
+    "3",
+    "3",
+    "3"
+    ]
+    },
+    {
+    "name": "fofofof",
+    "photo": "https://www.pexels.com/photo/grey-and-white-short-fur-cat-104827/",
+    "scores": [
+    "1",
+    "1",
+    "1",
+    "1",
+    "3",
+    "2",
+    "3",
+    "3",
+    "3",
+    "3"
+    ]
+    },
+    {
+    "name": "Crystal",
+    "photo": "https://placeimg.com/640/480/any",
+    "scores": [
+    "5",
+    "3",
+    "3",
+    "3",
+    "1",
+    "4",
+    "5",
+    "3",
+    "3",
+    "5"
+    ]
+    },
+    {
+    "name": "hyep",
+    "photo": "https://www.w3schools.com/images/colorpicker.gif",
+    "scores": [
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1"
+    ]
+    },
+    {
+    "name": "Sara",
+    "photo": "www.google.com",
+    "scores": [
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "2",
+    "1",
+    "1",
+    "1",
+    "1"
+    ]
+    },
+    {
+    "name": "xzczDc",
+    "photo": "sadfdsafdsaf",
+    "scores": [
+    "3",
+    "3",
+    "3",
+    "3",
+    "3",
+    "4",
+    "3",
+    "5",
+    "4",
+    "2"
+    ]
+    },
+    {
+    "name": "asdf",
+    "photo": "asdf",
+    "scores": [
+    "2",
+    "3",
+    "2",
+    "2",
+    "1",
+    "4",
+    "1",
+    "2",
+    "2",
+    "1"
+    ]
+    },
+    {
+    "name": "Tim",
+    "photo": "aisudhfjasjidf",
+    "scores": [
+    "2",
+    "4",
+    "2",
+    "4",
+    "3",
+    "4",
+    "3",
+    "3",
+    "3",
+    "2"
+    ]
+    },
+    {
+    "name": "john",
+    "photo": "https://loremflickr.com/320/240",
+    "scores": [
+    "3",
+    "4",
+    "4",
+    "4",
+    "5",
+    "5",
+    "5",
+    "5",
+    "5",
+    "5"
+    ]
+    }
+    ];
   
   // Note how we export the array. This makes it accessible to other files using require.
   module.exports = friends;

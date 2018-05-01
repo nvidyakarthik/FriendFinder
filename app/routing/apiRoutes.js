c7d143c074ee67c1465e5c36b3db10adc18f7944
nvidya.karthik <nvidya.karthik@gmail.com>
2018-05-01 09:23:45 -0700

    added logic to match friend and clear list added

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,20 +1,49 @@
 
-var friendsList = require("../data/friends");
+var friendsList = require("../data/friends.js");
 module.exports = function (app) {
 
     app.get("/api/friends", function (req, res) {
         res.json(friendsList);
     });
 
-    app.post("/post/friends", function (req, res) {
+    function indexOfSmallest(a) {
+        var lowest = 0;
+        for (var i = 1; i < a.length; i++) {
+         if (a[i] < a[lowest]) 
+            lowest = i;
+        }
+        return lowest;
+       }
+
+    app.post("/api/friends", function (req, res) {
+        console.log(req.body);
+        console.log(friendsList);
+       // req.body.scores=req.body.scores.map(Number);
         friendsList.push(req.body);
-        res.json(true);
+        var currentUser=req.body.scores;
+        var totalDifference=0;
+        var totalDiffArray=[];
+        friendsList.forEach(function(element,index){
+            totalDifference=0;
+            for(var i=0;i<currentUser.length;i++){
+                totalDifference+=Math.abs(Number(currentUser[i])-Number(element.scores[i]));
+            }
+            totalDiffArray.push(totalDifference);
+        });
+
+        var matchIndex=indexOfSmallest(totalDiffArray);
+        console.log(totalDiffArray);      
+        res.json(friendsList[matchIndex]);  
+        
+        
+       // res.json(true);
     });
 
-    app.post("/api/clear", function () {
-        // Empty out the arrays of data
-        friendsList = []
+    app.get("/api/clear", function (req,res) {
+       // Empty out the arrays of data
+        friendsList = [];
+        res.json(friendsList);
 
-        console.log(friendsList);
+        //console.log(friendsList);
     });
 };

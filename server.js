d6bef685630bea936717e4f9754c86e6873b0cc8
nvidya.karthik <nvidya.karthik@gmail.com>
2018-05-01 09:28:08 -0700

    corrected the routes in require

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ app.use(bodyParser.json());
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 // =============================================================================
 // LISTENER
@@ -34,4 +34,4 @@ app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
 
-
+ 
